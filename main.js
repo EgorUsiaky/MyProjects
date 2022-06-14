12e26059b131cb8202a87ac938d43327b8790f73
EgorUsiaky <81643944+EgorUsiaky@users.noreply.github.com>
2022-06-14 12:15:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const list = document.getElementById("list");
+const sites = ["Antools", "Coffee", "Knifes", "Mountains", "Music"];
+for (let i = 0; i < sites.length; i++) {
+    const site = sites[i];
+    const link = `<li><a href="sites/${site}/index.html" target="_blank">${site}</a></li>`;
+    list.insertAdjacentHTML("beforeend", link);
+}
