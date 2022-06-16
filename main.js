aea0e5998360269edf1828375fa45b186c98dae7
EgorUsiaky <81643944+EgorUsiaky@users.noreply.github.com>
2022-06-16 11:18:31 +0200

    edit main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const list = document.getElementById("list");
-const sites = ["Antools", "Coffee", "Knifes", "Mountains", "Music"];
+const sites = ["Antools", "Coffee", "Knifes", "Mountains", "Music", "Paper"];
 for (let i = 0; i < sites.length; i++) {
     const site = sites[i];
     const link = `<li><a href="sites/${site}/index.html" target="_blank">${site}</a></li>`;
